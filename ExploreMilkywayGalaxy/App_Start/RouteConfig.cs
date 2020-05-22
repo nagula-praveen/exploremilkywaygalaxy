8c2f1f49eae372b905abb17f19b53a0c69d65fc4
Nagula Praveen <nagula.praveen@outlook.com>
2020-05-22 15:09:46 +0530

    Adding Milkywau Controller

--- ExploreMilkywayGalaxy/App_Start/RouteConfig.cs
+++ ExploreMilkywayGalaxy/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ExploreMilkywayGalaxy
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "MilkywayHome", action = "Home", id = UrlParameter.Optional }
             );
         }
     }
